fa545b4326df5632f09018a639c62bcbd18abef4
Grigory Kotov <tomfun1990@gmail.com>
2014-09-08 21:25:24 +0300

    requirejs begining

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Tommy\Pymnt\MainBundle\TommyPymntMainBundle(),
+            new Hearsay\RequireJSBundle\HearsayRequireJSBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Tommy/Pymnt/MainBundle/Resources/assets/js/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by tomfun on 08.09.14.
+ */
+define([], function() {
+        //return an object to define the "my/shirt" module.
+        alert(111);
+    }
+);
